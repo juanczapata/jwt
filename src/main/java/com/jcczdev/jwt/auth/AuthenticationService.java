23e716b82263c4eb6108ab225a0aa646e0672029
Juan Carlos <jcarczapata@gmail.com>
2023-02-16 18:24:54 -0500

    Initial commit

---
+++ src/main/java/com/jcczdev/jwt/auth/AuthenticationService.java
@@ -0,0 +1,42 @@
+package com.jcczdev.jwt.auth;
+
+import com.jcczdev.jwt.config.JwtService;
+import com.jcczdev.jwt.user.Role;
+import com.jcczdev.jwt.user.User;
+import com.jcczdev.jwt.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserRepository repository;
+    private final PasswordEncoder encoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .password(encoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        repository.save(user);
+        var jstToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jstToken).build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate( new UsernamePasswordAuthenticationToken(
+                request.getEmail(), request.getPassword()));
+        var user = repository.findByEmail(request.getEmail()).orElseThrow();
+        var jstToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jstToken).build();
+    }
+}
---
+++ src/main/java/com/jcczdev/jwt/user/Role.java
@@ -0,0 +1,6 @@
+package com.jcczdev.jwt.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
